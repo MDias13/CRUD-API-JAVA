12cf632d5fc9b4e4dd780828a20004927182f850
Marcos Dias <59514474+MDias13@users.noreply.github.com>
2023-01-03 18:22:04 -0300

    Implementado Métodos CRUD
    GET , POST, PUT, DELETE

--- src/main/java/com/crud/api/controller/apiController.java
+++ src/main/java/com/crud/api/controller/apiController.java
@@ -5,7 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,19 +27,67 @@ public class apiController {
 	
 	
 	@GetMapping(value = "/listar")
-	@ResponseStatus(HttpStatus.OK)
+	@ResponseStatus(HttpStatus.OK) /*Retorna os dados para o corpo da resposta*/
 	public ResponseEntity<List<Usuario>> listar(){
 		
 		List<Usuario> usuarios = null;
 		try {
-			usuarios = usuarioRepository.findAll();
+			usuarios = usuarioRepository.findAll(); /*Executa a consulta no banco */
 		} catch (Exception e) {
 	
 			e.printStackTrace();
 		}
 		
-		return new ResponseEntity<List<Usuario>>(usuarios,HttpStatus.OK);
+		return new ResponseEntity<List<Usuario>>(usuarios,HttpStatus.OK); /*Retorna a lsita em JSON */
 		
 	}
-
+	
+	@PostMapping("/salvar")
+	@ResponseBody
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){ /*Recebe os dados para salvar */
+		
+		Usuario user = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(user,HttpStatus.CREATED);
+	}
+	
+	
+	@DeleteMapping("/deletar")
+	@ResponseBody
+	public ResponseEntity<String> deletar(@RequestParam Long id){ /*Recebe os dados para salvar */
+	
+		usuarioRepository.deleteById(id);
+		
+		return new ResponseEntity<String>("Usuario Deletado com Sucesso",HttpStatus.OK);
+	}
+	
+	@GetMapping("/buscar/{id}")
+	@ResponseBody
+	public ResponseEntity<Usuario> buscar(@PathVariable(name = "id") Long id){ /*Recebe os dados para salvar */
+	
+		Usuario user = usuarioRepository.findById(id).get();
+		
+		return new ResponseEntity<Usuario>(user,HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	@ResponseBody
+	public ResponseEntity<?> update(@RequestBody Usuario usuario){ /*Recebe os dados para salvar */
+	
+		if(usuario.getId() == null) {
+		
+		return new ResponseEntity<String>("ID não informado",HttpStatus.OK);
+		}
+		
+		Usuario user = usuarioRepository.saveAndFlush(usuario);
+		
+		return new ResponseEntity<Usuario>(user,HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/crud/api/repository/UsuarioRepository.java
+++ src/main/java/com/crud/api/repository/UsuarioRepository.java
@@ -1,11 +1,10 @@
 package com.crud.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.crud.api.model.Usuario;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 
 }
